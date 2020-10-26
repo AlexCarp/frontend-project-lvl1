09abe627dcae8cdab90fb2dc07a22d42484cc42f
Aliaksandr Ivanou <alexiva@yandex-team.ru>
2020-10-26 23:24:17 +0300

    Ask user for treir name

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,3 +1,10 @@
 #!/usr/bin/env node
+import promptly from 'promptly';
 
 console.log('Welcome to the Brain Games!');
+
+(async () => {
+    const name = await promptly.prompt('May I have your name?');
+
+    console.log(`Hello, ${name}!`);
+})();
